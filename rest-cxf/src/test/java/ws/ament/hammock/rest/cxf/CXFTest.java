f6208a01b399b16cc79fe6a33269faf1181ed0e5
John D. Ament <johndament@apache.org>
2016-08-22 19:53:36 -0400

    #18 Adding some tests to rest before adding configurable URIs.

---
+++ rest-cxf/src/test/java/ws/ament/hammock/rest/cxf/CXFTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 John D. Ament
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.ament.hammock.rest.cxf;
+
+import org.apache.cxf.cdi.CXFCdiServlet;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URI;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.is;
+
+@Ignore
+@RunWith(Arquillian.class)
+public class CXFTest {
+    @Deployment
+    public static JavaArchive createArchive() {
+        return ShrinkWrap.create(JavaArchive.class).addClasses(RestController.class, RestApp.class, CXFCdiServlet.class);
+    }
+
+    @ArquillianResource
+    private URI uri;
+
+    @Test
+    public void shouldBeAbleToRetrieveRestEndpoint() throws Exception {
+        get(uri + "/rest").then().assertThat().statusCode(200)
+                .body(is("Hello, World!"));
+    }
+}
