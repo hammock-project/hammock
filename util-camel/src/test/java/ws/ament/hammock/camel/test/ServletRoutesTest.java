443225e6b4f0c5cc38c6de37c319ca1de61c6bec
John D. Ament <johndament@apache.org>
2017-04-09 19:56:59 -0400

    Fixes #84 Introduce a Camel Component for simplified CDI based bootstrap.
    Signed-off-by: John D. Ament <johndament@apache.org>

---
+++ util-camel/src/test/java/ws/ament/hammock/camel/test/ServletRoutesTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Hammock and its contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.ament.hammock.camel.test;
+
+import io.restassured.RestAssured;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ws.ament.hammock.camel.servlet.HammockCamelServlet;
+
+import javax.inject.Inject;
+
+import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+public class ServletRoutesTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClasses(HammockCamelServlet.class, ServletRoutesTest.class, TestCamelRoutes.class, BasicProcessor.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    private BasicProcessor basicProcessor;
+
+    @Test
+    public void doSomething() throws Exception{
+        get("/camel/hello/").then().statusCode(200);
+        assertThat(basicProcessor.getHits()).isEqualTo(1);
+    }
+}
---
+++ util-camel/src/test/java/ws/ament/hammock/camel/test/TestCamelRoutes.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Hammock and its contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.ament.hammock.camel.test;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class TestCamelRoutes extends RouteBuilder {
+    @Inject
+    private BasicProcessor basicProcessor;
+    @Override
+    public void configure() throws Exception {
+        from("servlet://hello").process(basicProcessor);
+    }
+}
