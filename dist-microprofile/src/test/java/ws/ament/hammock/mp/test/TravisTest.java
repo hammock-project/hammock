c783b0bb4583fa7a3277e612b77707253bd3148c
John D. Ament <john.d.ament@gmail.com>
2017-08-12 22:43:16 -0400

    Adding test to debug travis.
    Signed-off-by: John D. Ament <john.d.ament@gmail.com>

--- dist-microprofile/src/test/java/ws/ament/hammock/mp/test/TravisTest.java
+++ dist-microprofile/src/test/java/ws/ament/hammock/mp/test/TravisTest.java
@@ -59,8 +59,13 @@ public class TravisTest extends Arquillian{
     public void testEnvironmentConfigSource() {
         Map<String, String> env = System.getenv();
         for (Map.Entry<String, String> envEntry : env.entrySet()) {
-            Assert.assertEquals("Discovered key "+envEntry.getKey()+" with value "+envEntry.getValue(),
-                    envEntry.getValue(), config.getValue(envEntry.getKey(), String.class));
+            try {
+                Assert.assertEquals("Discovered key "+envEntry.getKey()+" with value "+envEntry.getValue(),
+                        envEntry.getValue(), config.getValue(envEntry.getKey(), String.class));
+            }
+            catch (Throwable t) {
+                Assert.fail("Discovered key "+envEntry.getKey()+" with value "+envEntry.getValue(), t);
+            }
         }
     }
 }
