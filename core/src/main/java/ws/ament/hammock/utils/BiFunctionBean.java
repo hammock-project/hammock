6fbcd7471ff89f6db5b585be0fb14a8d55119db4
John D. Ament <john.d.ament@gmail.com>
2017-09-02 19:54:43 -0400

    #105 Adding utilities for a bean that is created from a bi function, as well as a wrapper for injection points.
    Signed-off-by: John D. Ament <john.d.ament@gmail.com>

---
+++ core/src/main/java/ws/ament/hammock/utils/BiFunctionBean.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017 Hammock and its contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.ament.hammock.utils;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+
+public class BiFunctionBean<T> implements Bean<Object> {
+    private final Set<Annotation> qualifiers;
+    private final Class<? extends Annotation> scope;
+    private final Class<?> beanType;
+    private final Set<Type> types;
+    private final T metadata;
+    private final BiFunction<CreationalContext<Object>, T, Object> biFunction;
+
+    public BiFunctionBean(Class<?> beanType, Type type, T metadata, Set<Annotation> qualifiers,
+                          Class<? extends Annotation> scope,
+                          BiFunction<CreationalContext<Object>, T, Object> biFunction) {
+        this.qualifiers = qualifiers;
+        this.scope = scope;
+        this.metadata = metadata;
+        this.biFunction = biFunction;
+        this.types = singleton(type);
+        this.beanType = beanType;
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return emptySet();
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+        return this.beanType;
+    }
+
+    @Override
+    public boolean isNullable() {
+        return false;
+    }
+
+    @Override
+    public Object create(CreationalContext<Object> context) {
+        return biFunction.apply(context, metadata);
+    }
+
+    @Override
+    public void destroy(Object instance, CreationalContext<Object> context) {
+
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return qualifiers;
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return scope;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        return emptySet();
+    }
+
+    @Override
+    public boolean isAlternative() {
+        return false;
+    }
+}
---
+++ core/src/main/java/ws/ament/hammock/utils/EmptyInjectionPoint.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 Hammock and its contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.ament.hammock.utils;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+
+public class EmptyInjectionPoint implements InjectionPoint {
+    private final Bean bean;
+
+    public EmptyInjectionPoint(Bean bean) {
+        this.bean = bean;
+    }
+
+    @Override
+    public boolean isTransient() {
+        return false;
+    }
+
+    @Override
+    public boolean isDelegate() {
+        return false;
+    }
+
+    @Override
+    public Type getType() {
+        return InjectionPoint.class;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return Collections.singleton(Default.Literal.INSTANCE);
+    }
+
+    @Override
+    public Member getMember() {
+        return null;
+    }
+
+    @Override
+    public Bean<?> getBean() {
+        return bean;
+    }
+
+    @Override
+    public Annotated getAnnotated() {
+        return null;
+    }
+}
